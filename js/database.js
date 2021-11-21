a15650cecf67c3cf4c0df27bd5e5ff119a031aea
sanidhya <sanidhya.varshney_cs19@gla.ac.in>
2021-11-21 13:22:19 +0530

    s commit

---
+++ js/database.js
@@ -0,0 +1,14 @@
+let databaseRef = firebase.database();
+
+
+
+//write new user inb the database
+function writeNewUser(userID, displayname, email, emailVerified){
+  databaseRef.ref('users/' + userID).set({
+    username:displayname,
+    email:email,
+    emailVerified: emailVerified
+  }).then( e => {
+    window.location.href ='index.html';
+  });
+}
---
+++ js/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAAW9v-16c65uhim3H8Db0aB4jLegWsegA",
+    authDomain: "shopping-cart-e1b8a.firebaseapp.com",
+    databaseURL: "https://shopping-cart-e1b8a-default-rtdb.firebaseio.com",
+    projectId: "shopping-cart-e1b8a",
+    storageBucket: "shopping-cart-e1b8a.appspot.com",
+    messagingSenderId: "495964716232",
+    measurementId: "G-D6EEDW1NCS"
+  };
+    firebase.initializeApp(firebaseConfig);
+  
